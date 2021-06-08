fc02853722c5a0f890bac7702d509fd08a520fa7
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-06-08 21:52:24 +0300

    Initial Commit

---
+++ AddASearchbarInSwiftUI/AddASearchbarInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddASearchbarInSwiftUIApp.swift
+//  AddASearchbarInSwiftUI
+//
+//  Created by Ramill Ibragimov on 08.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AddASearchbarInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
